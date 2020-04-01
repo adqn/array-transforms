7be4dd28ee826e4620469a46965b7482ec1456fd
adqn <57331120+adqn@users.noreply.github.com>
2020-04-01 15:13:01 -0700

    Update transformation_grid.py
    added counter-clockwise rotation

--- transformation_grid.py
+++ transformation_grid.py
@@ -189,6 +189,89 @@ class Transformation_Grid:
           temp_grid = self.shift_left(temp_grid, l, self.dist_from_axis + k, k)
 
     return temp_grid
+
+
+  # Counter-clockwise 
+  def shift45deg_ccw(self, grid, quadrant):
+    temp_grid = []
+    temp_grid.extend(range(len(grid)))
+
+    for i in temp_grid:
+      temp_grid[i] = []
+      for j in range(len(grid)):
+        temp_grid[i].append(0)
+
+
+    # Shift up by k subarrays
+    if quadrant == 1:
+      dist_reverse_last = self.indices_reverse.copy()[: self.dist_from_axis]
+      
+      for i in range(self.dist_from_axis):
+        j = dist_reverse_last[i]
+        k = j - self.dist_from_axis
+        
+        if grid[self.dist_from_axis][j] == 1:
+          temp_grid[self.dist_from_axis][j] = 1
+          temp_grid = self.shift_up(temp_grid, self.dist_from_axis, j, k)
+        
+        if grid[self.dist_from_axis + k][j] == 1:
+          temp_grid[self.dist_from_axis + k][j] = 1
+          temp_grid = self.shift_up(temp_grid, self.dist_from_axis + k, j, k)                
+
+        
+    # Shift left by k positions
+    if quadrant == 2:
+      for i in range(self.dist_from_axis):
+        j = self.dist_reverse[i]
+        k = abs(j - self.dist_from_axis)
+        
+        # Middle elements
+        if grid[j][self.dist_from_axis] == 1:
+          temp_grid[j][self.dist_from_axis] = 1
+          temp_grid = self.shift_left(temp_grid, j, self.dist_from_axis, k)
+          
+        # Diagonal elements
+        if grid[j][self.dist_from_axis + k] == 1:
+          temp_grid[j][self.dist_from_axis + k] = 1
+          temp_grid = self.shift_left(temp_grid, j, self.dist_from_axis + k, k)
+        
+    
+    # Shift down by k subarrays
+    if quadrant == 3:      
+      for i in range(self.dist_from_axis):
+        j = self.dist_reverse[i]
+        k = abs(j - self.dist_from_axis)
+
+        # Middle elements
+        if grid[self.dist_from_axis][j] == 1:
+          temp_grid[self.dist_from_axis][j] = 1
+          temp_grid = self.shift_down(temp_grid, self.dist_from_axis, j, k)
+
+        # Diagonal elements
+        if grid[self.dist_from_axis - k][j] == 1:
+          temp_grid[self.dist_from_axis - k][j] = 1
+          temp_grid = self.shift_down(temp_grid, self.dist_from_axis - k, j, k)      
+
+          
+    # Shift right by k positions
+    # This one's a bit cheesy
+    if quadrant == 4:
+      dist_reverse_last = self.indices_reverse.copy()[:self.dist_from_axis]
+      
+      for i in range(self.dist_from_axis):
+        j = dist_reverse_last[i]
+        l = self.indices[self.dist_from_axis + 1:][i]
+        k = l - self.dist_from_axis
+        
+        if grid[l][self.dist_from_axis] == 1:
+          temp_grid[l][self.dist_from_axis] = 1
+          temp_grid = self.shift_right(temp_grid, l, self.dist_from_axis, k)
+          
+        if grid[l][self.dist_from_axis - k] == 1:
+          temp_grid[l][self.dist_from_axis - k] = 1
+          temp_grid = self.shift_right(temp_grid, l, self.dist_from_axis - k, k)
+
+    return temp_grid
   
     
   # Call clockwise shift on each quadrant
@@ -237,4 +320,41 @@ class Transformation_Grid:
 
   # Eventually
   def rotate_left(self):
-    True
+    grid = self.grid.copy()
+
+    temp_grid = []
+    temp_grid.extend(range(len(grid)))
+
+    for i in temp_grid:
+      temp_grid[i] = []
+      for j in range(len(grid)):
+        temp_grid[i].append(0)
+
+    grid1 = self.shift45deg_ccw(grid, 1)      
+    grid2 = self.shift45deg_ccw(grid, 2)
+    grid3 = self.shift45deg_ccw(grid, 3)
+    grid4 = self.shift45deg_ccw(grid, 4)
+
+    for i in range(self.width):
+      for j in range(self.width):
+        current_char = 0
+
+        if grid1[i][j] != 0:
+          current_char = grid1[i][j]
+        
+        if grid2[i][j] != 0:
+          current_char = grid2[i][j]
+
+        if grid3[i][j] != 0:
+          current_char = grid3[i][j]
+
+        if grid4[i][j] != 0:
+          current_char = grid4[i][j]
+
+        temp_grid[i][j] = current_char
+    
+    # Put center element back lol
+    temp_grid[self.width - 1 - self.dist_from_axis][self.width - 1 - self.dist_from_axis] = self.grid[self.width - 1 - self.dist_from_axis][self.width - 1 - self.dist_from_axis]
+
+    self.grid = temp_grid
+
